a96c04be2b9e842cf2331b3a26d421df2a6a1797
homonovus <Doody134@gmail.com>
2017-08-19 09:14:35 -0400

    aa

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "\e[1mcpplife\e[0m\nbased on bumet1's jslife\nenjoy i guess" << std::endl;
+
+	std::cin.ignore();
+
+	return 0;
+}
